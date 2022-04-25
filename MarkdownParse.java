a63f7c51b0abb4e3caca8957b7d4b2ce314e2d4b
Ryan Walsh <43913316+RWalsh299@users.noreply.github.com>
2022-04-24 17:09:00 -0700

    part6 update - almost all working

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,25 +12,31 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex); 
-            if (openBracket == -1){openBracket = currentIndex + 1;}
+            int openBracket = 0;
+            int closeBracket = 0;
+            int openParen = 0;
+            int closeParen = 0;
+            if(markdown.indexOf(")", currentIndex) != -1 && markdown.indexOf("[",currentIndex) != -1 && markdown.indexOf("]",currentIndex) != -1 && markdown.indexOf("(",currentIndex) != -1){
+               
+                openBracket = markdown.indexOf("[", currentIndex); 
+                if (openBracket == -1){openBracket = currentIndex + 1;}
 
-            int closeBracket = markdown.indexOf("]", openBracket);
-            if(closeBracket == -1){closeBracket = openBracket + 1;}
+                closeBracket = markdown.indexOf("]", openBracket);
+                if(closeBracket == -1){closeBracket = openBracket + 1;}
 
-            int openParen = markdown.indexOf("(", closeBracket);
-            if (openParen == -1){openParen = closeBracket+1;}
+                openParen = markdown.indexOf("(", closeBracket);
+                if (openParen == -1){openParen = closeBracket+1;}
 
-            int closeParen = markdown.indexOf(")", openParen);
-            if (closeParen == -1){closeParen = markdown.indexOf(".", openParen) + 3;}
-
+                closeParen = markdown.indexOf(")", openParen);
+                if (closeParen == -1){closeParen = markdown.indexOf(".", openParen) + 3;}
+            }else {break;}
 
             if (markdown.indexOf("!", 0)+1 == openBracket){
                 break; 
             }else{
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
-            currentIndex = closeParen + 1;   
+            currentIndex = closeParen + 1;       
         }
         return toReturn;
     }
